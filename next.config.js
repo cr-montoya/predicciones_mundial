29c112e42de3d8baf7dba5509fcd22e69395a420
Cristian Montoya <cristianmontoyar27@gmail.com>
2026-06-12 18:00:15 -0500

    fix: remove unnecessary Cloudflare next-on-pages adapter for Pages compatibility

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // For Cloudflare Pages, use the @cloudflare/next-on-pages adapter
-  experimental: {
-    instrumentationHook: true,
-  },
+  // Cloudflare Pages handles everything natively with Next.js
 }
 
-// If building for Cloudflare Pages, use the adapter
-if (process.env.CF_PAGES) {
-  const { withCloudflarePages } = require('@cloudflare/next-on-pages/next')
-  module.exports = withCloudflarePages()(nextConfig)
-} else {
-  module.exports = nextConfig
-}
+module.exports = nextConfig
